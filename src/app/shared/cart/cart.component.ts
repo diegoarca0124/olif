82b7221d694eaa0c0a6bf4e413ee63d832ec8881
Diego Arca <54607302+diegoarca02@users.noreply.github.com>
2026-08-05 16:07:48 -0500

    Nuevo proyecto

--- src/app/shared/cart/cart.component.ts
+++ src/app/shared/cart/cart.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   imports: [],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class CartComponent {
 
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CartComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   schemas: [ CUSTOM_ELEMENTS_SCHEMA]
